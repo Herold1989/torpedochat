925a44dbb2a0ef380c6e3b0cf9d12775b7929058
herold1989 <jens_herold@gmx.net>
2023-10-07 19:05:01 +0200

    add navbar (full screen) and add logo)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
+import Navbar from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         "min-h-screen font-sans antialiased grainy",
         inter.className
       )}>
+        <Navbar/>
         {children}
         </body>
     </html>
