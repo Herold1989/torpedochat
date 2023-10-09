fc6342883ff68bcc785f5941ba32f048b4046c69
herold1989 <jens_herold@gmx.net>
2023-10-09 13:11:25 +0200

    fixing db breakdown

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,18 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { redirect } from "next/navigation"
+import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
 
-const Page = () => {
-    const {getUser} = getKindeServerSession()
+const Page = async () => {
+    const { getUser } = getKindeServerSession()
     const user = getUser()
-
-    if(!user || user.id) redirect('/auth-callback?origin=dashboard')
-
-    return <div>{user.email}</div>
+  
+    if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+  
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id
+      }
+    })
 }
 
 export default Page
