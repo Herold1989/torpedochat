8ff1002293fe76c48fbe4a2dbb4eee0ecb285bd7
herold1989 <jens_herold@gmx.net>
2023-10-09 08:04:53 +0200

    fixing paths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
-import Navbar from './components/Navbar'
+import Navbar from '../components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
