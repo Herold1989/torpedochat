2d9b283f0553b93d228ce330857fe7b45f5ce186
herold1989 <jens_herold@gmx.net>
2023-10-11 21:34:46 +0200

    adding stuff to database and designing dashboard

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,33 +1,41 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { publicProcedure, router } from './trpc';
-import { TRPCError } from '@trpc/server';
-import { db } from '@/db';
- 
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { privateProcedure, publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
+
 export const appRouter = router({
-    authCallback: publicProcedure.query(async ()=> {
-        const {getUser} = getKindeServerSession()
-        const user = getUser()
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
 
-        if(!user.id || !user.email) throw new TRPCError({code: 'UNAUTHORIZED'})
-        // check if the user is in the database
-        const  dbUser = await db.user.findFirst({
-            where: {
-                id: user.id
-            }
-        })
+    if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
+    // check if the user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
 
-        if(!dbUser) {
-            // create user in db
-            await db.user.create({
-                data: {
-                    id: user.id, 
-                    email: user.email
-                }
-            })
-        }
+    if (!dbUser) {
+      // create user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
 
-        return { sucess: true }
+    return { sucess: true };
+  }),
+  getUserFiles: privateProcedure.query(async ({ctx}) => {
+    const { userId } = ctx
+    return await db.file.findMany({
+      where: {
+        userId
+      }
     })
-});
+  }),
+})
 
 export type AppRouter = typeof appRouter;
