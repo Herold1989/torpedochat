162bab446cce7896452cfd75e67abdce7556c693
herold1989 <jens_herold@gmx.net>
2023-10-13 08:22:06 +0200

    backend properties for file view

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,15 +1,35 @@
+import { db } from "@/db"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { notFound, redirect } from "next/navigation"
+
 interface PageProps {
     params: {
         fileid: string
     }
 }
 
-const Page = ({params}: PageProps) => {
+const Page = async ({params}: PageProps) => {
     // retrieve the file id
     const {fileid} = params
+
+    // Make sure user is logged in
+    const {getUser} = getKindeServerSession()
+    const user = getUser()
+
+    if(!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileid}`)
     // make database call
-    return (<div>{fileid}</div>
-)
+
+    const file = await db.file.findFirst({
+        where: {
+            id: fileid,
+            userId: user.id
+        }
+    })
+
+    if(!file) notFound()
+
+    return (<div>{fileid}</div>)
+
 }
 
 export default Page
