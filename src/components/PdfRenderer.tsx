a9a4d29f5c75a65284f226cf5869e06b100bb0ff
herold1989 <jens_herold@gmx.net>
2023-10-16 08:10:44 +0200

    finished pdf rendering

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -39,6 +39,9 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
   const [currPage, setCurrPage] = useState<number>(1);
   const [scale, setScale] = useState<number>(1);
   const [rotation, setRotation] = useState<number>(0);
+  const [renderedScale, setRenderedScale] = useState<number | null>(null)
+  
+  const isLoading = renderedScale !== scale
 
   const CustomPageValidator = z.object({
     page: z
@@ -141,11 +144,9 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
           <Button
             onClick={() => setRotation((prev) => prev + 90)}
             variant="ghost"
-            aria-label="rotate 90 degrees"
-          >
+            aria-label="rotate 90 degrees">
             <RotateCw className="h-4 w-4" />
           </Button>
-
           <PdfFullscreen fileUrl={url} />
         </div>
       </div>
@@ -168,14 +169,28 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
               }}
               onLoadSuccess={({ numPages }) => setNumPages(numPages)}
               file={url}
-              className="max-h-full"
-            >
-              <Page
+              className="max-h-full">
+              {isLoading && renderedScale ? <Page
                 width={width ? width : 1}
                 pageNumber={currPage}
                 scale={scale}
                 rotate={rotation}
-              />
+                key = {"@" + renderedScale}
+                /> : null}
+                <Page
+                className={cn(isLoading ? "hidden": "")}
+                width={width ? width : 1}
+                pageNumber={currPage}
+                scale={scale}
+                rotate={rotation}
+                key={"@" + scale}
+                loading = {
+                  <div className="flex justify-center">
+                    <Loader2 className="my-24 h-6 w-6 animate-spin"/>
+                  </div>
+                }
+                onRenderSuccess={() => setRenderedScale(scale)}
+                />
             </Document>
           </div>
         </Simplebar>
