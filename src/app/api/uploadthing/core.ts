0c60bd3f80d67cdfd31cb84a7b1f3f04073acdd4
herold1989 <jens_herold@gmx.net>
2023-10-24 08:35:46 +0200

    setting up qdrant - connection to qdrant worked, no err in try-catch

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,22 +1,25 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { PDFLoader} from "langchain/document_loaders/fs/pdf";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { getQdrantClient } from "@/app/lib/Qdrant";
+import { QdrantVectorStore } from "langchain/vectorstores/qdrant";
+import { log } from "console";
 
 const f = createUploadthing();
- 
+
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
+      const { getUser } = getKindeServerSession();
+      const user = getUser();
 
-      const { getUser } = getKindeServerSession()
-      const user = getUser()
+      if (!user || !user.id) throw new Error("Unauthorized");
 
-      if(!user || !user.id) throw new Error("Unauthorized")
- 
-      return { userId: user.id};
+      return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const createdFile = await db.file.create({
@@ -27,24 +30,73 @@ export const ourFileRouter = {
           url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
           uploadStatus: "PROCESSING",
         },
-      })
+      });
 
       try {
-        const response = await fetch(`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`)
-        const blob = await response.blob()
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        );
+        const blob = await response.blob();
 
-        const loader = new PDFLoader(blob)
-        const pageLevelDocs = await loader.load()
+        const loader = new PDFLoader(blob);
+        const pageLevelDocs = await loader.load();
 
-        const pagesAmt = pageLevelDocs.length
+        const pagesAmt = pageLevelDocs.length;
 
-        //vectorize and index entire document
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
 
-      } catch (err) {
+        // Qdrant part - Vector indexing PDF
+        const collectionName = "torpedochat";
+
+        const qdrant = await getQdrantClient();
+        const qdrantResponse = await qdrant.getCollections();
         
-      }
+        const collectionNames = qdrantResponse.collections.map(
+          (collection) => collection.name
+        );
 
+        if (collectionNames.includes(collectionName)) {
+          await qdrant.deleteCollection(collectionName);
+        }
+
+        await qdrant.createCollection(collectionName, {
+          vectors: {
+            size: 1536,
+            distance: "Cosine",
+          },
+          optimizers_config: {
+            default_segment_number: 2, // work here
+          },
+          replication_factor: 2, // work here
+        });
+
+        await QdrantVectorStore.fromDocuments(pageLevelDocs, embeddings, {
+          url: process.env.QDRANT_URL,
+          collectionName: collectionName,
+        });
+        
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (err) {
+        console.log(err);
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
--- src/app/lib/Qdrant.ts
+++ src/app/lib/Qdrant.ts
@@ -1,14 +1,15 @@
 import {QdrantClient} from '@qdrant/js-client-rest';
 
 // or connect to Qdrant Cloud
-export const client = new QdrantClient({
+{/*export const client = new QdrantClient({
     url: 'https://99ec845a-906f-422e-8758-7152b85acc60.us-east4-0.gcp.cloud.qdrant.io:6333',
     apiKey: process.env.QDRANT_API_KEY
-});
+});*/}
 
-{/*export const getQdrantClient = async () => {
+export const getQdrantClient = async () => {
     const client = new QdrantClient({
         url: 'https://99ec845a-906f-422e-8758-7152b85acc60.us-east4-0.gcp.cloud.qdrant.io:6333',
         apiKey: process.env.QDRANT_API_KEY
     })
-}*/}
+    return client
+}
