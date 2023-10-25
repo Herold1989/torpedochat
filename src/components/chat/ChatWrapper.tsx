3ad643ca2d7c1bf09cd0c4f3183108252f5d4840
herold1989 <jens_herold@gmx.net>
2023-10-25 07:43:12 +0200

    route not working correctly

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -6,6 +6,7 @@ import Messages from "./Messages";
 import { ChevronLeft, Loader2, XCircle } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
+import { ChatContextProvider } from "./ChatContext";
 
 interface ChatWrapperProps {
   fileId: string;
@@ -73,6 +74,7 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
     )
 
   return (
+    <ChatContextProvider fileId = {fileId}>
     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
       <div className="flex-1 justify-between flex flex-col mb-28">
         <Messages />
@@ -80,6 +82,7 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
 
       <ChatInput />
     </div>
+    </ChatContextProvider>
   );
 };
 
