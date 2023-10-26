58f9e757f942ebc03926a05060dac901445630d9
herold1989 <jens_herold@gmx.net>
2023-10-26 10:58:04 +0200

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           </p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          Talk to your <span className="text-blue-600">Documents</span> in seconds.
+          Talk to your <span className="text-blue-600"> Financial Documents</span> in seconds.
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
           TorpedoChat allows you to chat with any PDF document.
