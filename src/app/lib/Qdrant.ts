bb258656cf82426e4b54a2037149aa10e931441b
herold1989 <jens_herold@gmx.net>
2023-11-25 18:17:04 +0100

    ...

--- src/app/lib/Qdrant.ts
+++ src/app/lib/Qdrant.ts
@@ -2,8 +2,8 @@ import { QdrantClient } from '@qdrant/js-client-rest';
 
 export const getQdrantClient = () => {
     const client = new QdrantClient({
-        url: process.env.QDRANT_URL!,
-        apiKey: process.env.QDRANT_API_KEY!,
+        url: process.env.QDRANT_URL,
+        apiKey: process.env.QDRANT_API_KEY,
         port: 6333,
     })
     return client
