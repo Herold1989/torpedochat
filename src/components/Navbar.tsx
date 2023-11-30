9674fb52bd7c0464af15556eda69dc7962987428
herold1989 <jens_herold@gmx.net>
2023-11-30 20:16:46 +0100

    adding avatar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { buttonVariants } from "@/components/ui/button"
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server"
+import { LoginLink, RegisterLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { ArrowRight } from "lucide-react"
 import Image from "next/image";
+import UserAccountNav from "./UserAccountNav"
 
 const Navbar = () => {
+
+    const {getUser} = getKindeServerSession()
+    const user = getUser()
+   
     return (
         <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
@@ -16,7 +21,7 @@ const Navbar = () => {
                     </Link>
                     {/* Add mobile Navbar after authentification */}
                     <div className="hidden items-center space-x-4 sm:flex"></div>
-                    <>
+                    {!user ? <>
                     <Link href="/pricing" className={buttonVariants({
                         variant: "ghost",
                         size: "sm",
@@ -28,7 +33,17 @@ const Navbar = () => {
                     <RegisterLink className={buttonVariants({
                         size: "sm",
                     })}>Get started <ArrowRight className="ml-1.5 h-5 w-5"/></RegisterLink>
-                    </>
+                    </> : <><Link href="/dashboard" className={buttonVariants({
+                        variant: "ghost",
+                        size: "sm",
+                    })}>Dashboard</Link>
+                    {/* <UserAccountNav name = {
+                        !user.given_name || !user.family_name ? "Your Account" : ´${user.family_name}``
+                    }
+                    email = {user.email ?? ""}
+                    imageUrl = {user.picture ?? ""}
+                    /> */}
+                    </>}
                 </div>
             </MaxWidthWrapper>
         </nav>
