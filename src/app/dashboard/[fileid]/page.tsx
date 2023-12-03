11b8a921a4754e41529b3462706c1c851e17b22e
herold1989 <jens_herold@gmx.net>
2023-12-03 08:20:06 +0100

    minor bug fixing

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -2,6 +2,7 @@ import PdfRenderer from "@/components/PdfRenderer"
 import ChatWrapper from "@/components/chat/ChatWrapper"
 
 import { db } from "@/db"
+import { getUserSubscriptionPlan } from "@/lib/stripe"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { notFound, redirect } from "next/navigation"
 
@@ -31,22 +32,26 @@ const Page = async ({params}: PageProps) => {
 
     // if true, throw 404 error
     if(!file) notFound()
+    
+    const plan = await getUserSubscriptionPlan()
 
-    return (<div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
-        <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
-            {/* left side */}
-            <div className="flex-1 xl:flex">
-                <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-                    <PdfRenderer url={file.url}/> 
-                </div>
+    return (
+        <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
+          <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
+            {/* Left sidebar & main wrapper */}
+            <div className='flex-1 xl:flex'>
+              <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
+                {/* Main area */}
+                <PdfRenderer url={file.url} />
+              </div>
             </div>
-            {/* right side */}
-            <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-                <ChatWrapper fileId={file.id}/>
+    
+            <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
+              <ChatWrapper isSubscribed={plan.isSubscribed} fileId={file.id} />
             </div>
+          </div>
         </div>
-    </div>)
-
+      )
 }
 
 export default Page
